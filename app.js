f765daff44a33f73f14e4ab4e325bcde252ee2cc
eatapple1004 <frank.virus0325@gmail.com>
2026-01-18 23:32:53 +0900

    aa

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use("/api/paypal",   paypalWebhook);
 app.use("/api/shopify/webhook",  shopifyWebhook);
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log(`✅ Server running on port ${process.env.PORT}`);
+    console.log(`✅ Server running on port ${process.env.PORT}`);
 });
 
 app.get("/", (req, res) => {
